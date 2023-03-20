f518e8d1accc8eb9fef80484eed6eec00fa24341
harsh822 <hp996847@gmail.com>
2023-03-20 22:17:19 +0530

    Mongo URI moved to .env file

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 module.exports = {
-  url: "mongodb+srv://harsh822:38_B09am@cluster0.funp6ag.mongodb.net/CodeforcesRatingNewsLetter?retryWrites=true&w=majority",
+  url: process.env.MONGO_URI,
 };
