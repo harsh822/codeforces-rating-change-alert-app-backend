27dce4208da975796e379f42113f1d8de808b420
harsh822 <hp996847@gmail.com>
2023-03-22 23:11:14 +0530

    Comment some code

--- server.js
+++ server.js
@@ -16,15 +16,15 @@ app.use(function (req, res, next) {
     "Access-Control-Allow-Origin",
     "https://codeforcesratingchangealertapp.web.app"
   );
-  next();
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
   res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-  );
+  next();
+  // res.setHeader("Access-Control-Allow-Origin", "*");
+  // res.setHeader("Access-Control-Allow-Credentials", "true");
+  // res.setHeader("Access-Control-Max-Age", "1800");
+  // res.setHeader(
+  //   "Access-Control-Allow-Methods",
+  //   "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  // );
 });
 app.use(cors(corsOptions));
 // parse requests of content-type - application/json
@@ -37,7 +37,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to application." });
 });
 db.mongoose.set("strictQuery", false);
-console.log("DB URL", db.url);
+// console.log("DB URL", db.url);
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
